a839d558ab427da579fd4b3d6ee666114632144b
Mathieu De Coster <mth.decoster@gmail.com>
2015-06-11 14:26:20 +0200

    Header guards

--- src/examples/common/util.h
+++ src/examples/common/util.h
@@ -1,3 +1,6 @@
+#ifndef UTIL_HEADER
+#define UTIL_HEADER
+
 #define GLFW_INCLUDE_GLCOREARB // Needed for OpenGL 3.3 on OS X
 #include <GLFW/glfw3.h>
 
@@ -5,3 +8,5 @@
 #include <iostream>
 
 void error_callback(int error, const char* description);
+
+#endif
