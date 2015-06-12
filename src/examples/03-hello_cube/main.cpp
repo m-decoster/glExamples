56f6f38d9a083ce197fbf61a20bca15f2e9faf99
Mathieu De Coster <mth.decoster@gmail.com>
2015-06-12 11:41:59 +0200

    Rotate over time + z coordinate

--- src/examples/03-hello_cube/main.cpp
+++ src/examples/03-hello_cube/main.cpp
@@ -5,7 +5,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 const char* VERTEX_SRC = "#version 330 core\n"
-                          "layout(location=0) in vec2 position;"          // Vertex position (x, y)
+                          "layout(location=0) in vec3 position;"          // Vertex position (x, y, z)
                           "layout(location=1) in vec3 color;"             // Vertex color (r, g, b)
                           "layout(location=2) in vec2 texcoord;"          // Texture coordinate (u, v)
                           "uniform mat4 model;"
@@ -17,7 +17,7 @@ const char* VERTEX_SRC = "#version 330 core\n"
                           "{"
                           "    fColor = color;"                           // Pass color to fragment shader
                           "    fTexcoord = texcoord;"                     // Pass texcoord to fragment shader
-                          "    gl_Position = projection * view * model * vec4(position, 0.0, 1.0);"     // Place vertex at (x, y, 0, 1)
+                          "    gl_Position = projection * view * model * vec4(position, 1.0);"     // Place vertex at (x, y, z, 1) and then transform it according to the projection, view and model matrices
                           "}";
 
 const char* FRAGMENT_SRC = "#version 330 core\n"
@@ -48,7 +48,7 @@ int main(void)
     // vertex_clip = M_projection . M_view . M_model . vertex_local
     // Create the model matrix
     glm::mat4 model;
-    // Rotate just a bit
+    // Rotate just a bit (the vector indicates the axes on which to rotate)
     model = glm::rotate(model, -glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     // Create the view matrix
     glm::mat4 view;
@@ -105,11 +105,11 @@ int main(void)
 
     float vertices[] =
     {
-        // x   y      r     g     b     u     v
-        -0.5f, 0.5f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-        0.5f,  0.5f,  1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
-        0.5f, -0.5f,  1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f
+        // x   y      z     r     g     b     u     v
+        -0.5f, 0.5f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.5f,  0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        0.5f, -0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f
     };
 
     // Upload the vertices to the buffer
@@ -130,15 +130,16 @@ int main(void)
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Enable the vertex attributes and upload their data (see: layout(location=x))
+    // NOTE: We have added a z coordinate so this code has again changed!
     glEnableVertexAttribArray(0); // position
-    // 2 floats: x and y, but 7 floats in total per row
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
+    // 3 floats: x and y, but 8 floats in total per row
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
     glEnableVertexAttribArray(1); // color
-    // 3 floats: r, g and b, but 7 floats in total per row and start at the third one
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+    // 3 floats: r, g and b, but 8 floats in total per row and start at the 4th one
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(2); // texture coordinates
-    // 2 floats: u and v, but 7 floats in total per row and start at the sixth one
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+    // 2 floats: u and v, but 8 floats in total per row and start at the 7th one
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
 
     // We have now successfully created a drawable Vertex Array Object
 
@@ -158,6 +159,10 @@ int main(void)
     {
         // Clear
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Just for fun, rotate the cube over time
+        model = glm::rotate(model, 40.0f * glm::radians((float)glfwGetTime()), glm::vec3(1.0f, 0.0f, 0.0f));
+        glfwSetTime(0.0);
 
         // Upload the MVP matrices
         GLint modelUL = glGetUniformLocation(program, "model");
