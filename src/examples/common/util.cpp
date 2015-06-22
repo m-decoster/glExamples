e9ec320062e20f3a8cf89831eeff01443b5d002e
Mathieu De Coster <mth.decoster@gmail.com>
2015-06-22 10:25:50 +0200

    Skybox rendering

--- src/examples/common/util.cpp
+++ src/examples/common/util.cpp
@@ -104,6 +104,12 @@ GLuint loadCubeMap(const char* posX, const char* negX, const char* posY,
         glTexImage2D(textureTypes[i], 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
         SOIL_free_image_data(img);
     }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
     return tex;
 }
 
