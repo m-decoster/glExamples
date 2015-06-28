1dfa97292e097ff85073cbd6ad8375f13b702be7
Mathieu De Coster <mth.decoster@gmail.com>
2015-06-28 15:39:17 +0200

    Fix offsets and strides

--- src/examples/10-sprite_batching/spritebatcher.cpp
+++ src/examples/10-sprite_batching/spritebatcher.cpp
@@ -56,10 +56,10 @@ SpriteBatcher::SpriteBatcher()
     glEnableVertexAttribArray(4);
     glEnableVertexAttribArray(5);
     glEnableVertexAttribArray(6);
-    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, (void*)(12 * sizeof(float)), (void*)0);
-    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, (void*)(12 * sizeof(float)), (void*)(4 * sizeof(float)));
-    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, (void*)(12 * sizeof(float)), (void*)(8 * sizeof(float)));
-    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, (void*)(12 * sizeof(float)), (void*)(12 * sizeof(float)));
+    glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+    glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+    glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+    glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
 
     glBindVertexArray(0);
 }
