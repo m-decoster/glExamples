d4a01f6d45fca64f1c5da5acda7f85f17bb81b7c
Mathieu De Coster <mth.decoster@gmail.com>
2015-07-24 12:33:49 +0200

    Fix some errors

--- src/examples/14-shadows/main.cpp
+++ src/examples/14-shadows/main.cpp
@@ -48,60 +48,27 @@ const char* VERTEX_SRC = "#version 330 core\n"
                          "}";
 
 const char* FRAGMENT_SRC = "#version 330 core\n"
-                           "#define NUM_POINT_LIGHTS 3\n"
-                           "struct Sun"
-                           "{"
-                           "    vec3 dir;"
-                           "    vec3 ambient;"
-                           "    vec3 diffuse;"
-                           "    vec3 specular;"
-                           "};"
-                           "struct PointLight"
-                           "{"
-                           "    vec3 position;"
-                           "    vec3 att;" // x = constant, y = linear, z = quadratic
-                           "    vec3 ambient;"
-                           "    vec3 diffuse;"
-                           "    vec3 specular;"
-                           "};"
                            "in vec3 fNormal;"
                            "in vec3 fPosition;"
                            "in vec2 fTexCoords;"
                            "in vec4 fLightSpacePos;"
                            "out vec4 outputColor;"
-                           "layout(std140) uniform Light"
-                           "{"
-                           "    Sun sun;"
-                           "    PointLight pointLights[NUM_POINT_LIGHTS];"
-                           "};"
+                           "uniform vec3 lightPos;"
                            "uniform vec3 eye;"
                            "uniform sampler2D matDiffuse;"
-                           "uniform sampler2D matSpecular;"
                            "uniform sampler2D shadowMap;"
                            "uniform float matShine;"
-                           "vec3 dirLight(Sun sun, vec3 normal, vec3 eye, float shdw)" // NEW
+                           "vec3 dirLight(vec3 lightPos, vec3 normal, vec3 eye, float shdw)" // NEW
                            "{"
-                           "    vec3 dir = normalize(-sun.dir);"
+                           "    vec3 dir = normalize(lightPos - fPosition);"
                            "    float diff = max(dot(normal, dir), 0.0);"
                            "    vec3 hwd = normalize(dir + eye);"
+                           "    vec3 color = vec3(1.0, 1.0, 1.0);"
                            "    float spec = pow(max(dot(normal, hwd), 0.0), matShine);"
-                           "    vec3 ambient = sun.ambient * vec3(texture(matDiffuse, fTexCoords));"
-                           "    vec3 diffuse = sun.diffuse * diff * vec3(texture(matDiffuse, fTexCoords));"
-                           "    vec3 specular = sun.specular * spec * vec3(texture(matSpecular, fTexCoords));"
-                           "    return ambient + (1.0 - shdw) * (diffuse + specular);" // NEW
-                           "}"
-                           "vec3 pointLight(PointLight light, vec3 normal, vec3 pos, vec3 eye, float shdw)" // NEW
-                           "{"
-                           "    vec3 dir = normalize(light.position - pos);"
-                           "    float diff = max(dot(normal, dir), 0.0);"
-                           "    vec3 hwd = normalize(dir + eye);"
-                           "    float spec = pow(max(dot(normal, hwd), 0.0), matShine);"
-                           "    float dist = length(light.position - pos);"
-                           "    float attenuation = 1.0f / (light.att.x + light.att.y * dist + light.att.z * dist * dist);"
-                           "    vec3 ambient = light.ambient * vec3(texture(matDiffuse, fTexCoords));"
-                           "    vec3 diffuse = light.diffuse * vec3(texture(matDiffuse, fTexCoords));"
-                           "    vec3 specular = light.specular * spec * vec3(texture(matSpecular, fTexCoords));"
-                           "    return (ambient + (1.0 - shdw) * (diffuse + specular)) * attenuation;" // NEW
+                           "    vec3 ambient = 0.3 * color * vec3(texture(matDiffuse, fTexCoords));"
+                           "    vec3 diffuse = color * diff * vec3(texture(matDiffuse, fTexCoords));"
+                           "    vec3 specular = 0.9 * spec * color;"
+                           "    return ambient + (1.0 - shdw) * (diffuse + specular);"
                            "}"
                            "float shadow(vec4 pos)" // NEW function
                            "{"
@@ -117,37 +84,35 @@ const char* FRAGMENT_SRC = "#version 330 core\n"
                            "    float shdw = shadow(fLightSpacePos);" // NEW
                            "    vec3 normal = normalize(fNormal);"
                            "    vec3 eyeDir = normalize(eye - fPosition);"
-                           "    vec3 result = dirLight(sun, normal, eyeDir, shdw);" // NEW
-                           "    for(int i = 0; i < NUM_POINT_LIGHTS; ++i)"
-                           "    {"
-                           "        result += pointLight(pointLights[i], normal, fPosition, eyeDir, shdw);" // NEW
-                           "    }"
+                           "    vec3 result = dirLight(lightPos, normal, eyeDir, shdw);" // NEW
                            "    outputColor = vec4(result, 1.0);"
                            "}";
 
 GLuint program, shadowProgram, vao, depthMap, depthMapFBO;
 glm::mat4 model, floorModel, lightSpace;
 Camera camera(CAMERA_PERSPECTIVE, 45.0f, 0.1f, 1000.0f, 640.0f, 480.0f);
-glm::vec3 lightPos(5.0f, 5.0f, 5.0f);
+glm::vec3 lightPos(0.0f, 10.0f, 0.0f);
+GLFWwindow* window;
 
 void shadowPass()
 {
     // Render the scene from the position of the directional light
-    glViewport(0, 0, SHADOW_W, SHADOW_H);
     glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+
+    glViewport(0, 0, SHADOW_W, SHADOW_H);
     glClear(GL_DEPTH_BUFFER_BIT);
 
     glm::mat4 lightProj, lightView;
     lightProj = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 1.0f, 7.5f);
-    lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+    lightView = glm::lookAt(lightPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     lightSpace = lightProj * lightView;
 
     glBindVertexArray(vao);
     glUseProgram(shadowProgram);
     glUniformMatrix4fv(glGetUniformLocation(shadowProgram, "lightSpace"), 1, GL_FALSE, glm::value_ptr(lightSpace));
-    glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(glGetUniformLocation(shadowProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
-    glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(floorModel));
+    glUniformMatrix4fv(glGetUniformLocation(shadowProgram, "model"), 1, GL_FALSE, glm::value_ptr(floorModel));
     glDrawArrays(GL_TRIANGLES, 0, 36);
     glBindVertexArray(0);
     glUseProgram(0);
@@ -157,14 +122,18 @@ void shadowPass()
 
 void geomPass()
 {
-    glViewport(0, 0, 640, 480);
+    // Reset the viewport (Also take care of HighDPI displays)
+    int w, h;
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0, 0, w, h);
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glBindVertexArray(vao);
     glUseProgram(program);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, depthMap);
-    glUniformMatrix4fv(glGetUniformLocation(shadowProgram, "lightSpace"), 1, GL_FALSE, glm::value_ptr(lightSpace));
+    glUniformMatrix4fv(glGetUniformLocation(program, "lightSpace"), 1, GL_FALSE, glm::value_ptr(lightSpace));
     glUniform3fv(glGetUniformLocation(program, "eye"), 1, glm::value_ptr(camera.getPosition()));
     glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -176,7 +145,6 @@ void geomPass()
 
 int main(void)
 {
-    GLFWwindow* window;
     window = init("Shadows", 640, 480);
     if(!window)
     {
@@ -300,47 +268,22 @@ int main(void)
     glBindBufferBase(GL_UNIFORM_BUFFER, 0, ubo);
     glBindBuffer(GL_UNIFORM_BUFFER, 0);
 
-    // Light ubo
-    GLuint lightUbo;
-    glGenBuffers(1, &lightUbo);
-    glBindBuffer(GL_UNIFORM_BUFFER, lightUbo);
-    // We have static lights in this example so we can fill the uniform buffer once and be done with it
-    float bufferData[] =
-    {
-        // sunlight
-        /* dir */ -0.5f, -0.5f, -0.5f, 0.0f, /* ambient */ 0.2f, 0.2f, 0.2f, 0.0f, /* diffuse */ 0.9f, 0.9f, 0.9f, 0.0f, /* specular */ 0.9f, 0.9f, 0.9f, 0.0f,
-        // pointlight 0
-        /* pos */ 0.0f, 5.0f, 0.0f, 0.0f, /* attenuation */ 1.0f, 0.7f, 1.8f, 0.0f, /* ambient */ 0.3f, 0.1f, 0.1f, 0.0f, /* diffuse */ 0.5f, 1.0f, 0.5f, 0.0f, /* specular */ 0.5f, 0.5f, 0.5f, 0.0f,
-        // pointlight 1
-        /* pos */ 5.0f, 5.0f, 0.0f, 0.0f, /* attenuation */ 1.0f, 0.35f, 0.44f, 0.0f, /* ambient */ 0.3f, 0.1f, 0.1f, 0.0f, /* diffuse */ 0.7f, 0.5f, 0.2f, 0.0f, /* specular */ 0.5f, 0.5f, 0.5f, 0.0f,
-        // pointlight 2
-        /* pos */ 0.0f, -5.0f, 5.0f, 0.0f, /* attenuation */ 1.0f, 0.35f, 0.44f, 0.0f, /* ambient */ 0.3f, 0.1f, 0.1f, 0.0f, /* diffuse */ 0.7f, 0.5f, 0.2f, 0.0f,/* specular */ 0.5f, 0.5f, 0.5f, 0.0f
-    }; // Vec3: we add a padding float after each vec3
-    glBufferData(GL_UNIFORM_BUFFER, sizeof(bufferData), bufferData, GL_STATIC_DRAW);
-    glBindBufferBase(GL_UNIFORM_BUFFER, 1, lightUbo);
-    glBindBuffer(GL_UNIFORM_BUFFER, 0);
-    GLuint light_index = glGetUniformBlockIndex(program, "Light");
-    glUniformBlockBinding(program, light_index, 1);
-
     int w, h;
     GLuint diffuse = loadImage("diffuseCube.png", &w, &h, 0, false);
     if(!diffuse)
     {
         return -1;
     }
-    GLuint specular = loadImage("specularCube.png", &w, &h, 1, false);
-    if(!specular)
-    {
-        return -1;
-    }
 
+    model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f));
     // The second cube is larger
     floorModel = glm::scale(floorModel, glm::vec3(100.0f, 1.0f, 100.0f));
     floorModel = glm::translate(floorModel, glm::vec3(0.0f, -5.0f, 0.0f));
 
     glUseProgram(program);
+    glUniform3f(glGetUniformLocation(program, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
     glUniform1i(glGetUniformLocation(program, "matDiffuse"), 0);
-    glUniform1i(glGetUniformLocation(program, "matSpecular"), 1);
+    glUniform1i(glGetUniformLocation(program, "shadowMap"), 1);
     glUniform1f(glGetUniformLocation(program, "matShine"), 32.0f);
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -359,8 +302,6 @@ int main(void)
 
         updateCamera(640, 480, window);
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         glBindBuffer(GL_UNIFORM_BUFFER, ubo);
         glBufferSubData(GL_UNIFORM_BUFFER, sizeof(glm::mat4), sizeof(glm::mat4), glm::value_ptr(camera.getView()));
         glBindBuffer(GL_UNIFORM_BUFFER, 0);
@@ -377,7 +318,6 @@ int main(void)
     glDeleteBuffers(1, &ubo);
     glDeleteVertexArrays(1, &vao);
     glDeleteTextures(1, &diffuse);
-    glDeleteTextures(1, &specular);
     glDeleteProgram(program);
     glDeleteProgram(shadowProgram);
 
