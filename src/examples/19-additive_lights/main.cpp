467ff7f04ebd0ae811ced9f9afb8c66c543a033e
Mathieu De Coster <mth.decoster@gmail.com>
2016-01-20 17:37:03 +0100

    stencils

--- src/examples/19-additive_lights/main.cpp
+++ src/examples/19-additive_lights/main.cpp
@@ -59,6 +59,20 @@ const char* FRAGMENT_LIGHT_SRC = "#version 330 core\n"
                                   "    outputColor = vec4(lightColor * attenuation, 1.0);"
                                   "}";
 
+const char* VERTEX_STENCIL_SRC = "#version 330 core\n"
+                                 "in vec2 position;"
+                                 "void main()"
+                                 "{"
+                                 "    gl_Position = vec4(position, 1.0, 1.0);"
+                                 "}";
+
+const char* FRAGMENT_STENCIL_SRC = "#version 330 core\n"
+                                   "out vec4 outputColor;"
+                                   "void main()"
+                                   "{"
+                                   "    outputColor = vec4(1.0);"
+                                   "}";
+
 struct PointLight
 {
     PointLight(const glm::vec3& pos, const glm::vec3& col, const glm::vec3& att)
@@ -67,6 +81,45 @@ struct PointLight
     glm::vec3 color;
     glm::vec3 attenuation;
 };
+
+// Don't actually draw quads like this
+void drawQuad(GLuint program, int* boundingBox, int width, int height)
+{
+    static bool firstRun = true;
+    static GLuint vao = 0;
+    if(firstRun)
+    {
+        glGenVertexArrays(1, &vao);
+    }
+    glBindVertexArray(vao);
+    static GLuint vbo = 0;
+    if(firstRun)
+    {
+        glGenBuffers(1, &vbo);
+    }
+    // Oh boy
+    float vertices[] = {
+        (float)boundingBox[0] / (width / 2) - 1, (float)boundingBox[1] / (height / 2) - 1, // top left
+        (float)boundingBox[2] / (width / 2) - 1, (float)boundingBox[1] / (height / 2) - 1, // top right
+        (float)boundingBox[0] / (width / 2) - 1, (float)boundingBox[3] / (height / 2) - 1, // bottom left
+        (float)boundingBox[0] / (width / 2) - 1, (float)boundingBox[3] / (height / 2) - 1, // bottom left
+        (float)boundingBox[2] / (width / 2) - 1, (float)boundingBox[1] / (height / 2) - 1, // top right
+        (float)boundingBox[2] / (width / 2) - 1, (float)boundingBox[3] / (height / 2) - 1, // bottom right
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    if(firstRun)
+    {
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    }
+    glUseProgram(program);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glUseProgram(0);
+    glBindVertexArray(0);
+    firstRun = false;
+}
 
 // Courtesy of http://learnopengl.com/#!Advanced-Lighting/Deferred-Shading
 // Calculate a light's radius based on its attenuation
@@ -173,7 +226,7 @@ int main(void)
     Camera camera(CAMERA_PERSPECTIVE, 45.0f, 0.1f, 1000.0f, 640.0f, 480.0f);
     setCamera(&camera);
 
-    GLuint zPassProgram, lightPassProgram;
+    GLuint zPassProgram, lightPassProgram, stencilProgram;
     {
         GLuint vertex = createShader(VERTEX_Z_PASS_SRC, GL_VERTEX_SHADER);
         GLuint fragment = createShader(FRAGMENT_Z_PASS_SRC, GL_FRAGMENT_SHADER);
@@ -194,6 +247,17 @@ int main(void)
         glDetachShader(lightPassProgram, vertex);
         glDeleteShader(vertex);
         glDetachShader(lightPassProgram, fragment);
+        glDeleteShader(fragment);
+    }
+    {
+        GLuint vertex = createShader(VERTEX_STENCIL_SRC, GL_VERTEX_SHADER);
+        GLuint fragment = createShader(FRAGMENT_STENCIL_SRC, GL_FRAGMENT_SHADER);
+        stencilProgram = createShaderProgram(vertex, fragment);
+        linkShader(stencilProgram);
+        validateShader(stencilProgram);
+        glDetachShader(stencilProgram, vertex);
+        glDeleteShader(vertex);
+        glDetachShader(stencilProgram, fragment);
         glDeleteShader(fragment);
     }
 
@@ -269,7 +333,6 @@ int main(void)
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_SCISSOR_TEST);
     // Additive blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_ONE, GL_ONE);
@@ -288,7 +351,6 @@ int main(void)
         glfwGetFramebufferSize(window, &width, &height);
 
         // 1. Z-PRE-PASS
-        glScissor(0, 0, width, height); // Reset the scissor for the Z-PRE-PASS
         glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); // Don't write colors
         glDepthMask(GL_TRUE); // Do depth writing
         glDepthFunc(GL_LESS);
@@ -302,6 +364,7 @@ int main(void)
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         // 2. LIGHTS
+        glEnable(GL_STENCIL_TEST);
         glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE); // Do color writing
         glDepthMask(GL_FALSE); // Do not write depth anymore
         glClear(GL_COLOR_BUFFER_BIT); // Only clear color buffer
@@ -313,20 +376,33 @@ int main(void)
         for(int i = 0; i < 3; ++i)
         {
             // Render the floor for each light
-            // Note that the vao is still bound from the z-pre-pass
-            // All we do is set the uniforms and call glDrawArrays
             // 
             // We optimize this part by only rendering the part of the screen that is affected by a light.
-            // We calculate the light's position in screen space and use glScissor to render only that part of the screen.
+            // We calculate the light's position in screen space and use the stencil buffer to render only that part of the screen.
             lightBBScreen(lights[i], camera.getProjection(), camera.getView(), width, height, boundingBox);
-            glScissor(boundingBox[0], boundingBox[1], boundingBox[2], boundingBox[3]);
+            // Update the stencil buffer
+            glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE); // Comment this line to see the contents of the stencil buffer
+            glDisable(GL_DEPTH_TEST);
+            glClear(GL_STENCIL_BUFFER_BIT); // Clear the stencil buffer
+            glStencilFunc(GL_ALWAYS, 1, 0xFF); // If a fragment is drawn, set it to 1
+            glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+            glStencilMask(0xFF); // Write to stencil buffer
+            drawQuad(stencilProgram, boundingBox, width, height);
+            glStencilMask(0x00); // No longer write to stencil buffer
+            glEnable(GL_DEPTH_TEST);
+            glStencilFunc(GL_EQUAL, 1, 0xFF); // If a fragment's stencil buffer value is 1, draw it
+            glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE); // Draw to the color buffer again
 
+            // Draw the geometry for the light
+            glUseProgram(lightPassProgram);
+            glBindVertexArray(vao);
             glUniform3fv(glGetUniformLocation(lightPassProgram, "lightPosition"), 1, glm::value_ptr(lights[i].position));
             glUniform3fv(glGetUniformLocation(lightPassProgram, "lightColor"), 1, glm::value_ptr(lights[i].color));
             glUniform3fv(glGetUniformLocation(lightPassProgram, "lightAtt"), 1, glm::value_ptr(lights[i].attenuation));
             // Additionally, we only render objects within the light radius (here: all objects = floor).
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+        glDisable(GL_STENCIL_TEST);
 
         glUseProgram(0);
         glBindVertexArray(0);
